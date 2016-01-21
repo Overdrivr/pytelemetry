4bca33cab03aadf93ace2fa32bc684b021e903b5
Rémi Bèges <remi.beges@gmail.com>
2016-01-21 16:43:17 +0100

    implemented serial transport

--- transports/serialtransport.py
+++ transports/serialtransport.py
@@ -1,18 +1,29 @@
+import serial
+
 class SerialTransport:
     def __init__(self):
-        pass
+        self.driver = None
 
-    # The transport interface
+    def connect(self, options):
+        print(options['port'])
+        self.driver = serial.Serial(port=options['port'],baudrate=options['baudrate'])
 
+    def disconnect(self):
+        self.driver.close()
+
+# The transport interface
     def read(self, maxbytes=1):
-        print("Trying to read at most ",maxbytes," characters.")
-        return 0 # TODO: Return char instead
+        in_waiting = self.driver.in_waiting
+        if in_waiting > maxbytes:
+            in_waiting = maxbytes
+        read = self.driver.read(size=in_waiting)
+        return read
 
     def readable(self):
-        return 0
+        return self.driver.in_waiting
 
     def write(self, data):
-        print("Asking to write ",data)
+        self.driver.write(data)
         return 0
 
     def writeable(self):
