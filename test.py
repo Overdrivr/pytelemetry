9863a1c5c5555e0cfc05ddbfd609834b482c3727
Rémi Bèges <remi.beges@gmail.com>
2016-01-21 16:42:31 +0100

    removed old test file

--- test.py
+++
@@ -1,8 +0,0 @@
-import pytelemetry as tm
-from transports.serialtransport import *
-
-transportMock = SerialTransport()
-
-c = tm.pytelemetry(transportMock)
-
-c.publish(b'sometopic',b'booyaa','string')
