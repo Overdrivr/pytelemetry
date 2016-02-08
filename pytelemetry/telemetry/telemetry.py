2ec6c6fb8e3b4cd7b398372251f04d9bf31a0653
Rémi Bèges <remi.beges@gmail.com>
2016-02-08 18:04:41 +0100

    Fixed interface issues

--- pytelemetry/telemetry/telemetry.py
+++ pytelemetry/telemetry/telemetry.py
@@ -1,5 +1,5 @@
-#from . import crc
-#from . import framing
+from .. import crc
+from .. import framing
 
 class Telemetry:
     """
@@ -8,14 +8,12 @@ class Telemetry:
     def __init__(self,transport, on_frame_callback):
         self.transport = transport
         self.on_frame_callback = on_frame_callback
+        self.types = ['string','uint8','uint16','uint32','int8','int16','int32','float32']
 
-    def publish(topic, data, datatype):
+    def publish(self,topic, data, datatype):
         pass
 
-    def subscribe(topic,callback):
-        pass
-
-    def update():
+    def update(self):
         pass
 
     def _emplace(msg,data):
