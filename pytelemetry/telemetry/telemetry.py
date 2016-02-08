963af082e01455900fc5fd776d7c690d96331f34
Rémi Bèges <remi.beges@gmail.com>
2016-02-08 17:50:36 +0100

    Base code for telemetry submodule

---
+++ pytelemetry/telemetry/telemetry.py
@@ -0,0 +1,21 @@
+#from . import crc
+#from . import framing
+
+class Telemetry:
+    """
+    Low level telemetry protocol (github.com/Overdrivr/Telemetry)
+    """
+    def __init__(self,transport):
+        pass
+
+    def publish(topic, data, datatype):
+        pass
+
+    def update():
+        pass
+
+    def emplace(msg,data):
+        pass
+
+    def subscribe(topic,callback):
+        pass
