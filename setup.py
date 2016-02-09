2c09621d3b23491c6b61bb3cbb824e1d029d7d59
Rémi Bèges <remi.beges@gmail.com>
2016-02-09 13:31:24 +0100

    Updated setup.py for release 1.1.0

--- setup.py
+++ setup.py
@@ -2,22 +2,19 @@ from setuptools import setup, find_packages, Extension
 from codecs import open
 from os import path
 from setuptools.dist import Distribution
+import pypandoc
 
 try:
-   import pypandoc
    description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
+except (IOError, ImportError) as e:
     print("ERROR : Could not convert README.md. Fallback")
+    print(e)
     description = open('README.md').read()
-
-class BinaryDistribution(Distribution):
-    def is_pure(self):
-        return False
 
 setup(
     name='pytelemetry',
 
-    version='1.0.1',
+    version='1.1.0',
 
     description='Lightweight remote monitoring and control of embedded devices',
     long_description=description, # Not working !
@@ -56,10 +53,6 @@ setup(
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['tests']),
 
-    # Workaround
-    include_package_data=True,
-    distclass=BinaryDistribution,
-
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
@@ -73,25 +66,5 @@ setup(
     extras_require={
         'dev': ['check-manifest'],
         'test': ['pytest','pytest-cov'],
-    },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={
-        'telemetry': ['crc16.dll','framing.dll','telemetry.dll']
-    },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-
     },
 )
