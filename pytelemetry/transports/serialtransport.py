f1648e46a8931386ce99ab7498eef34530b12429
Rémi Bèges <remi.beges@gmail.com>
2016-03-05 19:32:42 +0100

    Caught potential exceptions in serial driver

--- pytelemetry/transports/serialtransport.py
+++ pytelemetry/transports/serialtransport.py
@@ -1,8 +1,10 @@
 import serial
+from logging import getLogger
 
 class SerialTransport:
     def __init__(self):
         self.driver = None
+        self.log_rx = getLogger('telemetry.rx')
 
     def connect(self, options):
         self.driver = serial.Serial(port=options['port'],baudrate=options['baudrate'])
@@ -12,13 +14,29 @@ class SerialTransport:
 
 # The transport interface
     def read(self, maxbytes=1):
-        in_waiting = self.driver.in_waiting
+        try:
+            in_waiting = self.driver.in_waiting
+        except serial.SerialException as e:
+            self.log_rx.error("Caught Exception during read driver.in_waiting : %s" % e)
+            return None
+
         if in_waiting > maxbytes:
             in_waiting = maxbytes
-        read = self.driver.read(size=in_waiting)
+        try:
+            read = self.driver.read(size=in_waiting)
+        except serial.SerialException as e:
+            self.log_rx.error("Caught Exception during transport read : %s" % e)
+            return None
+
         return read
 
     def readable(self):
+        try:
+            in_waiting = self.driver.in_waiting
+        except serial.SerialException as e:
+            self.log_rx.error("Caught Exception during read driver.in_waiting : %s" % e)
+            return 0
+    
         return self.driver.in_waiting
 
     def write(self, data):
